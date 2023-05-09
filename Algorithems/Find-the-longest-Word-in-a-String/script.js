ddaa54c063e3a50c6d9ef31a70a148f2e1601bf0
Hassan-Shaif <131597544+Hassan-Shaif@users.noreply.github.com>
2023-05-09 09:21:00 +0200

    Add files via upload

--- Algorithems/Find-the-longest-Word-in-a-String/script.js
+++ Algorithems/Find-the-longest-Word-in-a-String/script.js
@@ -10,7 +10,7 @@
 //Pseudocode
 
 //-We will create a function to take string and variable to hold the character number of the longest word
-//-we will create a variable to hold every single word in a string
+//-we will create an array to hold every single word in a string
 //- We will create a for loop to loop through each word in words array
 //- We will create another loop to loop through each character in evey word to be able to count the length of every word
 //- if statement to check which word has the longest length and assign it to max
@@ -31,11 +31,18 @@ for (var i =0 ; i < words.length; i++){
    }
  return max;
 }
-console.log(findLongestWord("May the force be with you"));
+console.log(findLongestWord("May the force beeeeeeeeeeeeeeeeee with you"));
 
 //diagram
-//1- the function will take a string as an argument
-//2-the string will be split into words
+//1- The function will take a string
+//2-The string will be split into words and stored in array(words)
 //3-first for loop will loop inside the words array starting with first word (i=0)
-//4- second loop will loop through each character
-//5- if statement will check which word has the longest length and update max value
+//4- second loop will loop through each character in the first word starting with first character(when j = 0)
+//5- if statement will check if the length of the first word(i = 0) > max which is zero here. and assign the length value of
+//-the first word to max. Max here equal the length of first word.
+//6- the first loop will go again (when i = 1) and take the second word
+//7-second loop will loop through each character in the second word starting with first character(when j = 0)
+//8- if statement will check if the length of the second word(i = 1) > max which is the length of first word here,
+//-and assign the length value of the second word to max if its length > than first word length.
+//-...............the loop will take again the next word and the process will be the same until the last word
+//-Max value will be always updated if there is any other word that has length bigger than the existing one.
